8713a4133080748728d292ecac386140630093a4
mikoto20000 <mikoto20000@gmail.com>
2012-05-29 17:40:10 +0900

    Remove wasted code.

--- init.rb
+++ init.rb
@@ -1,20 +1,9 @@
 require 'redmine'
 require 'git_adapter_patch'
 
-begin
-  require 'dispatcher'
-  def dispatch(plugin, &block)
-    Dispatcher.to_prepare(plugin, &block)
-  end
-rescue LoadError # Rails 3
-  def dispatch(plugin, &block)
-    Rails.configuration.to_prepare(&block)
-  end
-end
-
 Redmine::Plugin.register :redmine_git_branch_hook do
   name 'Redmine Git Branch Hook plugin'
   author 'Takashi Okamoto'
   description 'Relate the commit to the issue from git branch name'
-  version '0.1.2'
+  version '0.2.1'
 end
